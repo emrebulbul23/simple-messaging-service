86461c2a4fc6ae1c544afd1075a612e6f3c962be
emrebulbul23 <emrebulbul23@gmail.com>
2020-09-12 15:25:04 +0300

    Initial commit

---
+++ src/main/java/com/interview/simplemessagingservice/SimpleMessagingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.interview.simplemessagingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMessagingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleMessagingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interview/simplemessagingservice/SimpleMessagingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.simplemessagingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleMessagingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
